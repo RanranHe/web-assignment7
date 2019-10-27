5b8d0c520f83920b91b70b1880c46bb75389afaf
Ranran He <cangxuexiao@gmail.com>
2019-10-27 14:12:39 -0400

    updated table structure

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.getElementById("submit").onclick = function () {
+    resetTable();
     let attributes;
     let content = document.getElementById('text').value;
     let apiKey = "b18c2oMvG8IIXhOBj1krVOCU5wXLXplSHquaJuBNLNQuaxyEH84PV3ZiPcjC";
@@ -9,30 +10,51 @@ document.getElementById("submit").onclick = function () {
     }).then(res => res.json())
         .then(json => {
             let arr = json.data;
+            console.log(arr.length);
             if (arr) {
                 attributes = Object.getOwnPropertyNames(arr[0]);
-                showResult(attributes, arr, resetTable(attributes));
+                showResult(arr, attributes);
             }
         });
 
-    function showResult(attributes, arr, callback) {
-        setTimeout(function () {
-            for (let i = 0; i < arr.length; i++) {
-                for (let j = 0; j < attributes.length; j++) {
-                    let name = `${attributes[j]}`;
-                    let innerHTML = document.getElementById(name).innerHTML;
-                    document.getElementById(name).innerHTML = innerHTML + '<td>' + arr[i][name] + '</td>';
-                }
+    // SNAP,TWTR,VOD,SNA,UPSN,SPU.F
+    function showResult(arr, attributes) {
+        for (let i = 0; i < arr.length; i++) {
+            let content = document.getElementById('table').innerHTML;
+            let id = "row_" + i;
+            console.log(id);
+            document.getElementById('table').innerHTML = content + `<tr id=${id}>` + '</tr>';
+            for (let j = 0; j < attributes.length; j++) {
+                let td = document.getElementById(`${id}`).innerHTML;
+                document.getElementById(`${id}`).innerHTML = td + '<td>' + arr[i][attributes[j]] + '</td>';
             }
-            callback && callback();
-        }, 50);
-    }
-
-    function resetTable(attributes) {
-        for (let j = 0; j < attributes.length; j++) {
-            let name = `${attributes[j]}`;
-            document.getElementById(name).innerHTML = '';
         }
     }
+
+    function resetTable() {
+        let header = document.getElementById('header');
+        document.getElementById('table').innerHTML = '';
+        document.getElementById('table').appendChild(header);
+    }
+
+    // function showResult(attributes, arr, callback) {
+    //     setTimeout(function () {
+    //         for (let i = 0; i < arr.length; i++) {
+    //             for (let j = 0; j < attributes.length; j++) {
+    //                 let name = `${attributes[j]}`;
+    //                 let innerHTML = document.getElementById(name).innerHTML;
+    //                 document.getElementById(name).innerHTML = innerHTML + '<td>' + arr[i][name] + '</td>';
+    //             }
+    //         }
+    //         callback && callback();
+    //     }, 50);
+    // }
+    //
+    // function resetTable(attributes) {
+    //     for (let j = 0; j < attributes.length; j++) {
+    //         let name = `${attributes[j]}`;
+    //         document.getElementById(name).innerHTML = '';
+    //     }
+    // }
 };
 
